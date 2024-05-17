e4b7019adf3a9c1f84b0e5e2369973f2f40c893a
Binod <binod20052001bkt@gmail.com>
2024-05-17 13:17:25 +0530

    Designed Header

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import "./Header.css"
+import logo from "../../assets/Quick_Foods_Logo.png"
 
-function Header(){
-    return(
-        <h1>This is header</h1>
+
+function Header() {
+    return (
+        <>
+            <div className="main">
+                <div className="content">
+                    <div className="logo-sec">
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className="menu-sec">
+                        <ul className="menues">
+                            <li className="menu1">Home</li>
+                            <li className="menu2">About Us</li>
+                            <li className="menu3">Contact Us</li>
+                            <li className="menu4">Services</li>
+                            <li className="menu5">Search</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
