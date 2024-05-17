80b9213a668e98bcda10faa556f5cf6dc92f9aa6
Binod <binod20052001bkt@gmail.com>
2024-05-17 13:17:25 +0530

    Designed Header

--- frontend/src/component/Contact_Us/Contact_Us.jsx
+++ frontend/src/component/Contact_Us/Contact_Us.jsx
@@ -4,8 +4,7 @@ import "./Contact_Us.css";
 function Contact_Us(){
     return(
         <>
-        <h1 class="Contact_heading">This is Contact Us</h1>
-        <h1 class="Contact_heading">This is Contact Us</h1>
+            <h1> This is contact Us</h1>
         </>
     )
 }
