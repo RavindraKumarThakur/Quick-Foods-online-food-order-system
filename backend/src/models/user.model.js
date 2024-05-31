04a38a8b387c3b7279d2d990b45e152f1aa63ecb
ravindra_thakur <kumarravindra54787@gmail.com>
2024-05-31 09:59:47 +0530

    Made jwt token and crypted the password entered by user

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
     userName: {
@@ -31,5 +32,16 @@ const userSchema = new Schema({
         default: 'MALE'
     }
 });
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password,10);
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
 
 export const User = mongoose.model("User",userSchema);
