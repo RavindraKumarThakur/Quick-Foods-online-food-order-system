85a05741c5a64ad392ec5d643d1b1bd7f9f9c91c
ravindra_thakur <kumarravindra54787@gmail.com>
2024-06-01 20:12:34 +0530

    Added jwt accesstokens and worked in header a little bit

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = async (req,res,next) => {
+try {
+        const token = req.cookies?.accessToken || req.headers('Authorization').split(" ")[1]
+    
+        if (!token) {
+            throw console.log("Unauthorized request");
+        }
+    
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken?._id).select("-password")
+    
+        if (!user) {
+            throw new Error("Unauthorized user");
+        }
+         req.user = user
+         next()
+} catch (error) {
+    throw new Error( error?.message || "Unauthorized request" );
+}
+}
--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -1,28 +1,64 @@
 import React from "react";
 import "./Header.css"
 import logo from "../../assets/Quick_Foods_Logo.png"
+import { Link ,NavLink } from "react-router-dom";
 
 
 function Header() {
     return (
-        <>
-            <div className="main">
-                <div className="content">
-                    <div className="logo-sec">
-                        <img src={logo} alt="" />
-                    </div>
-                    <div className="menu-sec">
-                        <ul className="menues">
-                            <li className="menu1">Home</li>
-                            <li className="menu2">About Us</li>
-                            <li className="menu3">Contact Us</li>
-                            <li className="menu4">Services</li>
-                            <li className="menu5">Search</li>
+        <header width="100%" bg-color="black" height="10vh">
+            <nav>
+                <div>
+                <Link to="/" className = "logo">
+                        <img
+                            src={logo}
+                            alt="Logo"
+                        />
+                    </Link>
+                    <div
+                        id="mobile-menu-2"
+                    >
+                        <ul>
+                            <li>
+                                <NavLink
+                                to="/"
+                                style={({isActive}) => (isActive? {color: "orangered"}: {color:"#3b3b3b"})}
+                                className= "menues"
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                to="/About_Us"
+                                style={({isActive}) => (isActive? {color: "orangered"}: {color:"#3b3b3b"})}
+                                className= "menues"
+                                >
+                                    About
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                to="/Contact_Us"
+                                style={({isActive}) => (isActive? {color: "orangered"}: {color:"#3b3b3b"})}
+                                className= "menues"
+                                >
+                                    Contact Us
+                                </NavLink>
+                            </li>
                         </ul>
                     </div>
+                    <div >
+                        <NavLink to="/login" className="loginBtn" style={({isActive}) => (isActive? {} : {})}>
+                            Log In
+                        </NavLink> 
+                        <NavLink to="/signup" className="signupBtn" style={({isActive}) => (isActive? {} : {})}>
+                            Sign Up
+                        </NavLink> 
+                    </div>
                 </div>
-            </div>
-        </>
+            </nav>
+        </header>
     )
 }
 
