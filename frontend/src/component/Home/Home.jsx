36e5ab6eeac6831c7b747177a5a160528ab50849
Binod <binod20052001bkt@gmail.com>
2024-07-10 21:05:20 +0530

    problem solve

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -3,10 +3,11 @@ import React from "react";
 function Home(){
     return(
         <>
+
         <h1>This is Home</h1>
-        <h1></h1>
+       
         </>
-    )
+    )   
 }
 
 export default Home
